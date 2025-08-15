f6d2f0c1d0c0db6f277f4387661b4a166b97a96c
Sergei Giniatulin <kornburn@gmail.com>
2025-08-15 21:00:23 +0300

    Updated version of the CatBoost integration code
    Including logging configuration

--- examples/catboost_integration/source/app.d
+++ examples/catboost_integration/source/app.d
@@ -1,4 +1,4 @@
-import std.stdio;
+import std.stdio : writeln;
 import std.conv : to;
 import std.csv : csvReader;
 
@@ -6,23 +6,52 @@ import chdb.session;
 import chdb.format;
 
 void main() {
-    auto session = &Session.sessionInstance();
-    session.connect(":memory:");
+    auto sess = &Session.sessionInstance();
+    sess.connect(":memory:");
 
-    auto res = session.query(`
-    SELECT
-        id,
-        count() AS row_count,
-        avg(feat_0) as avg_feat_0,
-        median(feat_1) as med_feat_1,
-        quantile(0.25)(feat_2) as quant_25_feat_2
-    FROM file("sample.csv", "CSV")
-    GROUP BY id
-    ORDER BY id
+    auto res = sess.query(`
+    CREATE TABLE amazon_train
+    (
+        date Date MATERIALIZED today(),
+        ACTION UInt8,
+        RESOURCE UInt32,
+        MGR_ID UInt32,
+        ROLE_ROLLUP_1 UInt32,
+        ROLE_ROLLUP_2 UInt32,
+        ROLE_DEPTNAME UInt32,
+        ROLE_TITLE UInt32,
+        ROLE_FAMILY_DESC UInt32,
+        ROLE_FAMILY UInt32,
+        ROLE_CODE UInt32
+    )
+    ENGINE = MergeTree ORDER BY tuple()
     `, OutputFormat.CSVWithNames);
+    writeln("Creation table error: ", res.error);
 
-    foreach (record; csvReader!(string[string])(res.buf, null))
-        writefln("client #%s: found %s times and has mean value of feature_0: %1.3f",
-                record["id"], record["row_count"],
-                record["avg_feat_0"].to!float);
+    res = sess.query(`INSERT INTO amazon_train FROM INFILE 'data/train.csv'`, OutputFormat.CSV);
+    writeln("Insert data error: ", res.error);
+
+    res = sess.query(`SELECT count() from amazon_train`, OutputFormat.CSV);
+    writeln("Select count error: ", res.error);
+
+    res = sess.query(`
+    SELECT
+        catboostEvaluate('cb_models/catboost_model.bin',
+                      RESOURCE,
+                      MGR_ID,
+                      ROLE_ROLLUP_1,
+                      ROLE_ROLLUP_2,
+                      ROLE_DEPTNAME,
+                      ROLE_TITLE,
+                      ROLE_FAMILY_DESC,
+                      ROLE_FAMILY,
+                      ROLE_CODE) > 0 AS prediction,
+        ACTION AS target
+    FROM amazon_train
+    LIMIT 10`, OutputFormat.CSV);
+    writeln("---");
+    writeln("Data: ", res.buf);
+    writeln("Time elapsed: ", res.elapsed);
+    writeln("Select count error: ", res.error);
+    writeln("---");
 }
